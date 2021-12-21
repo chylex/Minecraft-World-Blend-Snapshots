dad08f668255b593e8f2352c5282d852bddcaf25
chylex <contact@chylex.com>
2021-12-21 03:14:29 +0100

    Fix crash when trying to blend chunks before their biomes generated

--- src/main/java/chylex/worldblendsnapshots/mixin/EnableWorldBlendingMixin.java
+++ src/main/java/chylex/worldblendsnapshots/mixin/EnableWorldBlendingMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.chunk.ChunkStatus;
 import net.minecraft.world.level.chunk.storage.ChunkStorage;
 import net.minecraft.world.level.storage.DimensionDataStorage;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,15 +21,27 @@ public abstract class EnableWorldBlendingMixin {
 	
 	private static final String BLENDING_DATA_TAG = "blending_data";
 	private static final String OLD_NOISE_TAG = "old_noise";
+	private static final String STATUS_TAG = "Status";
 	
 	@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 	@Inject(method = "upgradeChunkTag", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;putInt(Ljava/lang/String;I)V"))
-	private void hookUpgradeChunkTag(ResourceKey<Level> world, Supplier<DimensionDataStorage> storage, CompoundTag tag, Optional<ResourceKey<Codec<? extends ChunkGenerator>>> key, CallbackInfoReturnable<CompoundTag> cir) {
+	private void addBlendingTag(ResourceKey<Level> world, Supplier<DimensionDataStorage> storage, CompoundTag tag, Optional<ResourceKey<Codec<? extends ChunkGenerator>>> key, CallbackInfoReturnable<CompoundTag> cir) {
 		final int version = ChunkStorage.getVersion(tag);
-		if (version < VERSION_1_18_1 && !tag.contains(BLENDING_DATA_TAG)) {
+		
+		if (version < VERSION_1_18_1 && !tag.contains(BLENDING_DATA_TAG) && ChunkStatus.byName(tag.getString(STATUS_TAG)).isOrAfter(ChunkStatus.BIOMES)) {
 			CompoundTag bd = new CompoundTag();
 			bd.putBoolean(OLD_NOISE_TAG, true);
 			tag.put(BLENDING_DATA_TAG, bd);
+		}
+	}
+	
+	@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+	@Inject(method = "upgradeChunkTag", at = @At("RETURN"))
+	private void removeBrokenBlendingTag(ResourceKey<Level> world, Supplier<DimensionDataStorage> storage, CompoundTag tag, Optional<ResourceKey<Codec<? extends ChunkGenerator>>> key, CallbackInfoReturnable<CompoundTag> cir) {
+		final int version = ChunkStorage.getVersion(tag);
+		
+		if (version >= VERSION_1_18_1 && tag.contains(BLENDING_DATA_TAG) && !ChunkStatus.byName(tag.getString(STATUS_TAG)).isOrAfter(ChunkStatus.BIOMES)) {
+			tag.remove(BLENDING_DATA_TAG);
 		}
 	}
 }
